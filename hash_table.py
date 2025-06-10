f5c0a46009ed7f91beeb2dea1f5bc4c21fca0fa6
Vedrakholia <vedrakholia32@gmail.com>
2025-06-10 15:38:14 -0400

    Other methods included

--- hash_table.py
+++ hash_table.py
@@ -13,6 +13,44 @@ class HashTable:
             print(i, ": ", val)
 
 
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+    
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+
+        return all_keys
+
+
+
 my_hash_table = HashTable()
 
+my_hash_table.set_item('bolts', 140)
+my_hash_table.set_item('washers', 120)
+my_hash_table.set_item('lumber', 690)
 my_hash_table.print_table()
+
+print("After appling get item: ")
+
+print(my_hash_table.get_item("bolts"))
+print(my_hash_table.get_item("washers"))
+print(my_hash_table.get_item("something"))
+
+print("After appling Keys method: ")
+
+print(my_hash_table.keys())
